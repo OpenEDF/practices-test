ddef28500f99578400b28c83b8636b5da0bc5c37
macro <makermuyi@163.com>
2021-08-31 23:03:00 +0800

    add the riscv core code

--- riscv-core-code/wq70xx.h
+++ riscv-core-code/wq70xx.h
@@ -1,8 +1,8 @@
-#ifndef __WQ70XX__H__
-#define __WQ70XX__H__
+#ifndef __TESTXX__H__
+#define __TESTXX__H__
 
 /* memory address */
 #define GPIO_BASEADDR   *(unsigned int *)(0x01440000)
 #define I2C0_BASEADDR   *(unsigned int *)(0x01600000)
 
-#endif /* __WQ70XX__H__ */
+#endif /* __TESTXX__H__ */
