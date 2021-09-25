478afcc4d2997a39e0139ce61008950d4dd37921
macro <makermuyi@163.com>
2021-09-25 17:43:41 +0800

    add the vcs + verdi + dve to .basrc

--- shell-script/my_bashrc.sh
+++ shell-script/my_bashrc.sh
@@ -29,3 +29,30 @@ curl https://wttr.in/shanghai-pudong
 
 # gio open view pdf document
 alias openpdf="gio open"
+
+# VSC + VERDI + SCL
+export DVE_HOME=/home/macro/synopsys/vcs_2018/vcs/O-2018.09-SP2
+export VCS_HOME=/home/macro/synopsys/vcs_2018/vcs/O-2018.09-SP2
+export LD_LIBRARY_PATH=/home/macro/synopsys/verdi_2018/verdi/Verdi_O-2018.09-SP2/share/PLI/VCS/LINUX64
+export VERDI_HOME=/home/macro/synopsys/verdi_2018/verdi/Verdi_O-2018.09-SP2
+export SCL_HOME=/home/macro/synopsys/scl_2018/scl/2018.06
+
+# DVE
+PATH=$PATH:$VCS_HOME/gui/dve/bin
+alias dve='dve -full64'
+
+# VCS
+PATH=$PATH:$VCS_HOME/bin
+alias vcs='vcs'
+
+# VERDI
+PATH=$PATH:$VERDI_HOME/bin
+alias verdi="verdi"
+
+# SCL
+PATH=$PATH:$SCL_HOME/linux64/bin
+export VCS_ARCH_OVERRIDE=linux
+
+# LICENCE
+export LM_LICENSE_FILE=27000@macro-ubuntu
+alias lmg_synopsys='lmgrd -c /home/macro/synopsys/scl_2018/scl/2018.06/admin/license/Synopsys.dat'
