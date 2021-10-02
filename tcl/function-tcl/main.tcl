1d45d172c76a3e5cb99b1a1e23aad6ad0238647c
macro <makermuyi@163.com>
2021-10-02 23:07:31 +0800

    add the tcl global example

--- tcl/function-tcl/main.tcl
+++ tcl/function-tcl/main.tcl
@@ -6,6 +6,8 @@
 #
 
 set glo 10
+set ::c 2   ;# global veriable
+set d 30
 
 proc main {} {
     puts "Hello World!"
@@ -16,12 +18,22 @@ proc add {a b} {
 }
 
 proc avg {number} {
-    set sum [expr $number + 10]
+    set sum [expr $number + $::c]
     main
     return $sum
+}
+
+proc gloarg {} {
+    global d
+    set d [expr 10 + $d]
+    return $d
 }
 
 # run main
 main
 puts [add 10 30]
-puts [avg $glo]
+puts [avg 10]
+puts $c
+puts $d
+puts [ gloarg ]
+puts $d
