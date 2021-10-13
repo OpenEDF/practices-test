7c2184a61345a8b9171bbc320e814c64af0699ab
macro <makermuyi@163.com>
2021-10-13 19:58:34 +0800

    add pyrhon print example

---
+++ python-re/print.py
@@ -0,0 +1,23 @@
+#!/bin/python
+# print 
+intvalue = 10
+float_value = 12.5
+str_value = "Hello World"
+
+# print 1
+print("intvalue = %d. float_value = %f. str_value = %s" % (intvalue, float_value, str_value))
+
+# print 2
+print("3 + 5 = %d." % (3 + 5))
+#intvalue = 10. float_value = 12.500000. str_value = Hello World
+#3 + 5 = 8.
+
+# print 3
+print("float 2 = %.2f\n" % (3.141592654))
+# float 2 = 3.14
+
+# print 4
+a = 3.14
+b = "macro"
+print("a = {0}, b = {1}" .format(a, b))
+# a = 3.14, b = macro
