50aa5867228d8e367d7ba33becee4d8897b0fd3b
macro <makermuyi@163.com>
2021-10-14 08:40:19 +0800

    add Design compile and spyglass to .bashrc

--- shell-script/my_bashrc.sh
+++ shell-script/my_bashrc.sh
@@ -1,42 +1,146 @@
-#scons completion
-#. $(find -name sconscompletion.sh)
-. /usr/share/bash-completion/scons_user
+# ~/.bashrc: executed by bash(1) for non-login shells.
+# see /usr/share/doc/bash/examples/startup-files (in the package bash-doc)
+# for examples
 
-# spike and pk
-#alias pk='/opt/riscv/riscv64-unknown-elf/bin/pk'
-export PATH=$PATH:/opt/riscv/spike/bin
+# If not running interactively, don't do anything
+case $- in
+    *i*) ;;
+      *) return;;
+esac
 
-alias cd1..='cd ..'
-alias cd2..='cd ../..'
-alias cd3..='cd ../../..'
-alias cd4..='cd ../../../..'
-alias cd5..='cd ../../../../..'
-alias cd6..='cd ../../../../../..'
-alias cd7..='cd ../../../../../../'
+# don't put duplicate lines or lines starting with space in the history.
+# See bash(1) for more options
+HISTCONTROL=ignoreboth
 
-# git psuh hawk
-alias push_hawk='git push origin HEAD:refs/for/hawk_v3_dev'
-alias clone_hawk='echo ssh://yang.zhixiong@192.168.100.51:29419/hawk_open_core.git'
-HAWK_REP='ssh://yang.zhixiong@192.168.100.51:29419/hawk_open_core.git'
+# append to the history file, don't overwrite it
+shopt -s histappend
 
-# system-c support
-SYSTEMC_HOME=/usr/local/systemc233
-SYSTEMC_INCLUDE=$SYSTEMC_HOME/include
-SYSTEMC_LIBDIR=$SYSTEMC_HOME/lib-linux64
+# for setting history length see HISTSIZE and HISTFILESIZE in bash(1)
+HISTSIZE=1000
+HISTFILESIZE=2000
 
-# query the weather
-curl https://wttr.in/shanghai-pudong
+# check the window size after each command and, if necessary,
+# update the values of LINES and COLUMNS.
+shopt -s checkwinsize
+
+# If set, the pattern "**" used in a pathname expansion context will
+# match all files and zero or more directories and subdirectories.
+#shopt -s globstar
+
+# make less more friendly for non-text input files, see lesspipe(1)
+[ -x /usr/bin/lesspipe ] && eval "$(SHELL=/bin/sh lesspipe)"
+
+# set variable identifying the chroot you work in (used in the prompt below)
+if [ -z "${debian_chroot:-}" ] && [ -r /etc/debian_chroot ]; then
+    debian_chroot=$(cat /etc/debian_chroot)
+fi
+
+# set a fancy prompt (non-color, unless we know we "want" color)
+case "$TERM" in
+    xterm-color|*-256color) color_prompt=yes;;
+esac
+
+# uncomment for a colored prompt, if the terminal has the capability; turned
+# off by default to not distract the user: the focus in a terminal window
+# should be on the output of commands, not on the prompt
+#force_color_prompt=yes
+
+if [ -n "$force_color_prompt" ]; then
+    if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
+	# We have color support; assume it's compliant with Ecma-48
+	# (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+	# a case would tend to support setf rather than setaf.)
+	color_prompt=yes
+    else
+	color_prompt=
+    fi
+fi
+
+if [ "$color_prompt" = yes ]; then
+    PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+else
+    PS1='${debian_chroot:+($debian_chroot)}\u@\h:\w\$ '
+fi
+unset color_prompt force_color_prompt
+
+# If this is an xterm set the title to user@host:dir
+case "$TERM" in
+xterm*|rxvt*)
+    PS1="\[\e]0;${debian_chroot:+($debian_chroot)}\u@\h: \w\a\]$PS1"
+    ;;
+*)
+    ;;
+esac
+
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto'
+    #alias dir='dir --color=auto'
+    #alias vdir='vdir --color=auto'
+
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
+# colored GCC warnings and errors
+#export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+
+# some more ls aliases
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+# Add an "alert" alias for long running commands.  Use like so:
+#   sleep 10; alert
+alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
+
+# Alias definitions.
+# You may want to put all your additions into a separate file like
+# ~/.bash_aliases, instead of adding them here directly.
+# See /usr/share/doc/bash-doc/examples in the bash-doc package.
+
+if [ -f ~/.bash_aliases ]; then
+    . ~/.bash_aliases
+fi
+
+# enable programmable completion features (you don't need to enable
+# this, if it's already enabled in /etc/bash.bashrc and /etc/profile
+# sources /etc/bash.bashrc).
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    . /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    . /etc/bash_completion
+  fi
+fi
+
+# set the esp-idf env
+alias espenv='. $HOME/esp/esp-idf/export.sh'
+alias python='python3'
+
+# set the user command
+alias ..="cd .."
+alias ..2="cd ../.."
+alias ..3="cd ../../.."
+alias ..4="cd ../../../.."
+alias ..5="cd ../../../../.."
+
+# risc-v toochain
+PATH=/opt/riscv/bin:$PATH
 
 # gio open view pdf document
 alias openpdf="gio open"
 
-# VSC + VERDI + SCL
+# VSC + VERDI + SCL + SPYGLASS + DC
 export DVE_HOME=/home/macro/synopsys/vcs_2018/vcs/O-2018.09-SP2
 export VCS_HOME=/home/macro/synopsys/vcs_2018/vcs/O-2018.09-SP2
 export LD_LIBRARY_PATH=/home/macro/synopsys/verdi_2018/verdi/Verdi_O-2018.09-SP2/share/PLI/VCS/LINUX64
 export VERDI_HOME=/home/macro/synopsys/verdi_2018/verdi/Verdi_O-2018.09-SP2
 export SCL_HOME=/home/macro/synopsys/scl_2018/scl/2018.06
-
+export SPYGLASS_HOME=/home/macro/synopsys/spyglass_2016/SpyGlass-L2016.06/SPYGLASS_HOME
+export DC_HOME=/home/macro/synopsys/dc_2016/syn/L-2016.03-SP1/
 # DVE
 PATH=$PATH:$VCS_HOME/gui/dve/bin
 alias dve='dve -full64'
@@ -53,6 +157,18 @@ alias verdi="verdi"
 PATH=$PATH:$SCL_HOME/linux64/bin
 export VCS_ARCH_OVERRIDE=linux
 
+# SPYGLASS
+PATH=$PATH:$SPYGLASS_HOME/bin
+alias spy='spyglass'
+
+#DC
+PATH=$PATH:$DC_HOME/bin
+alias dc='dc_shell'
+alias dv='design_vision'
+
 # LICENCE
 export LM_LICENSE_FILE=27000@macro-ubuntu
 alias lmg_synopsys='lmgrd -c /home/macro/synopsys/scl_2018/scl/2018.06/admin/license/Synopsys.dat'
+
+# Vivado
+#source xilinx/Vivado/2019.2/settings64.sh
