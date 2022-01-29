9efd4bf191e2042659f6fc5fa7b5da8dcc79a4cf
macro <makermuyi@163.com>
2022-01-29 18:30:52 +0800

    Add the script for linux kernel debug

---
+++ linux-kernel-gdb/run_vm.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Boot the emulated system to a shell prompt.
+
+QUEM=qemu-system-x86_64
+
+run_emulator()
+{
+  [ ! -z "$DEBUG" ] && set -x
+  $QUEM -nographic \
+        -kernel arch/x86_64/boot/bzImage \
+        -append "console=ttyS0,115200 nokaslr" \
+        -gdb tcp::2222 \
+        -S 
+}
+
+if [ "$1" != "--norun" ]
+then
+  run_emulator
+fi
