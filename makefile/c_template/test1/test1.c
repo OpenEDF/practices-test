3960678e8607c9a22bdbe6618e7aefab3954ca1d
macro <makermuyi@163.com>
2023-08-29 21:25:30 +0800

    add makefile test

---
+++ makefile/c_template/test1/test1.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int test1(void)
+{
+    printf(" this is a test test dir!\n");
+    return 0;
+}
---
+++ makefile/c_template/test2/test2.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int test2(void)
+{
+    printf(" this is a test2!\n");
+    return 0;
+}
