8364b81c3dd9f51d806a171b23257ccd5d7b7b32
macro <makermuyi@163.com>
2023-08-31 00:01:06 +0800

    fix: verilog define macros

--- sync/tb.v
+++ sync/tb.v
@@ -1,10 +1,15 @@
 module tb;
 integer fd;
-parameter string str = "add";
 initial begin
 	fd = $fopen("mhome_inst_test.rpt", "a+b");
-	$fdisplay(fd, "mhome inst test: %s ... OK", str);
+	$fdisplay(fd, "mhome inst test: %s ... OK", `INST_NAME);
 	$fclose(fd);
 end
 
 endmodule
+
+// iverilog -D INST_NAME=\"jar\" -o test tb.v
+// iverilog -D INST_NAME=\"add\" -o test tb.v
+//
+// vpp test
+// cat mhome_inst_test.rpt
